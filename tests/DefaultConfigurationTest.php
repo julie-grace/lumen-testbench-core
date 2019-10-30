db9e6d58f9531a9ad4d845287e27ef2ccdd5524b
Darryn Ten <darryn.ten@gmail.com>
2019-10-30 16:19:09 +0700

    Exclude Session tests

--- tests/DefaultConfigurationTest.php
+++ tests/DefaultConfigurationTest.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace Orchestra\Testbench\Tests;
+namespace Lumen\Testbench\Tests;
 
-use Orchestra\Testbench\TestCase;
+use Lumen\Testbench\TestCase;
 
 class DefaultConfigurationTest extends TestCase
 {
@@ -10,11 +10,5 @@ class DefaultConfigurationTest extends TestCase
     public function it_populate_expected_cache_defaults()
     {
         $this->assertEquals('array', $this->app['config']['cache.default']);
-    }
-
-    /** @test */
-    public function it_populate_expected_session_defaults()
-    {
-        $this->assertEquals('array', $this->app['config']['session.driver']);
     }
 }
